01389e9ddddd77d382454ec5d8c0c5a15deda7dd
y4ga <yagamimeister@gmail.com>
2016-09-27 14:00:14 +0400

    Project Creation
    Project created with gdx-setup.jar

---
+++ android/src/com/fidelm/game_of_life/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.fidelm.game_of_life;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.fidelm.game_of_life.GameOfLife;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameOfLife(), config);
+	}
+}
---
+++ html/src/com/fidelm/game_of_life/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.fidelm.game_of_life.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.fidelm.game_of_life.GameOfLife;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GameOfLife();
+        }
+}
